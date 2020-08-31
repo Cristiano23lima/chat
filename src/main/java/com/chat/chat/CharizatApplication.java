91914579bbaeb6a6122d54549eca66c5fcd436ff
devCristiano <cristiano.rodrigues@f5promotora.com.br>
2020-08-31 12:39:39 -0300

    init commit

---
+++ src/main/java/com/chat/chat/CharizatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharizatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharizatApplication.class, args);
+	}
+
+}
